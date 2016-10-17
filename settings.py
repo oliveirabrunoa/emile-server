68aeac617afd6e47d660686ac179ac433f7aeb88
EliakinCosta <eliakim170@gmail.com>
2016-10-17 14:59:45 -0300

    Comment path to mysql backend

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), '')
+DB_PATH = os.path.join(os.path.dirname(__file__), 'test.db')
 
 AUTHENTICATION_BACKEND = 'ldapauthenticationbackend.LDAPAuthenticationBackend'
-BACKEND_PATH = 'mysql://eliakimdjango:mnk21jie1892ud9@eliakimdjango.mysql.pythonanywhere-services.com/eliakimdjango$default'
+BACKEND_PATH = 'sqlite:///{}'.format(DB_PATH)
+#BACKEND_PATH = 'mysql://eliakimdjango:mnk21jie1892ud9@eliakimdjango.mysql.pythonanywhere-services.com/eliakimdjango$default'
