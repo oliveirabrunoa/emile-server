619454c63e095d4e1340badf11a152ee99fe2b51
EliakinCosta <eliakim170@gmail.com>
2016-10-17 14:45:59 -0300

    Test to mysql

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), 'test.db')
+DB_PATH = os.path.join(os.path.dirname(__file__), '')
 
 AUTHENTICATION_BACKEND = 'ldapauthenticationbackend.LDAPAuthenticationBackend'
-BACKEND_PATH = 'sqlite:///{}'.format(DB_PATH)
+BACKEND_PATH = 'mysql://eliakimdjango:mnk21jie1892ud9@eliakimdjango.mysql.pythonanywhere-services.com/eliakimdjango$default'
