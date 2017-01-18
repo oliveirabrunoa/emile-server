ec4ed419e8716f3c4b647a30497b786fb5190d16
EliakinCosta <eliakim170@gmail.com>
2017-01-18 17:06:17 -0300

    Initial dato to one user - test

---
+++ load_data.py
@@ -0,0 +1,38 @@
+import importlib
+import json
+from emile_server import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+# an Engine, which the Session will use for connection
+# resources
+some_engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+
+
+# create a configured "Session" class
+Session = sessionmaker(bind=some_engine)
+
+
+# create a Session
+session = Session()
+
+
+def load_data():
+    with open('./initial_data/data.json') as data_file:
+        data = json.load(data_file)
+
+        for item in data:
+            model_relative_path = item['model']
+            module_path, model_name = str(model_relative_path).rsplit('.', maxsplit=1)
+            model = getattr(importlib.import_module(module_path), model_name)
+
+            kwargs = item['fields']
+            object = model()
+            object.set_fields(kwargs)
+            session.add(object)
+            session.commit()
+
+
+if __name__=='__main__':
+    load_data()
