a33ed6f88b35e09302acd13086b7664237968014
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-01-19 15:21:12 -0300

    Bug fix - delete_user

--- cruds/crud_course_section_students/models.py
+++ cruds/crud_course_section_students/models.py
@@ -3,9 +3,9 @@ from cruds.crud_courses.models import Courses
 
 
 class CourseSectionStudents(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    course_section_id = db.Column(db.Integer, db.ForeignKey('course_sections.id'), primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    course_section_id = db.Column(db.Integer, db.ForeignKey('course_sections.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     course_section = db.relationship("CourseSections")
 
     def serialize(self):
