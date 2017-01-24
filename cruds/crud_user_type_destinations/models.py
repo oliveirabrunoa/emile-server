f6c494bc39f6733efa8702bf18570733644d8cbe
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-01-24 14:27:54 -0300

    Criação do model UserTypeDestinations

--- cruds/crud_user_type_destinations/models.py
+++ cruds/crud_user_type_destinations/models.py
@@ -3,16 +3,22 @@ from backend import db
 from cruds.crud_user_type.models import UserType
 
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class UserTypeDestinations(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50))
+    param_values_service = db.Column(db.String(250))
+    users_query  = db.Column(db.String(250))
 
 
     def serialize(self):
         return {
             'id': self.id,
+            'name':self.name,
+            'param_values_service':self.param_values_service,
+            'users_query': self.users_query
         }
 
     def set_fields(self, fields):
-        self.id = fields['username']
-        self.birth_date = datetime.datetime.strptime(fields['birth_date'], "%m-%d-%Y").date()
-        
+        self.name = fields['name']
+        self.param_values_service = fields['param_values_service']
+        self.users_query = fields['users_query']
