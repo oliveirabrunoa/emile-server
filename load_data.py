c9770d9d5449e0666e1e0fd402701ca073002e98
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-02-15 15:53:27 -0300

    Refatoração do script de carga inicial de dados e separação dos arquivos por model

--- load_data.py
+++ load_data.py
@@ -3,6 +3,7 @@ import json
 from emile_server import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
 
 # an Engine, which the Session will use for connection
@@ -19,19 +20,25 @@ session = Session()
 
 
 def load_data():
-    with open('./initial_data/data.json') as data_file:
-        data = json.load(data_file)
 
-        for item in data:
-            model_relative_path = item['model']
-            module_path, model_name = str(model_relative_path).rsplit('.', maxsplit=1)
-            model = getattr(importlib.import_module(module_path), model_name)
+    files = sorted(os.listdir(os.getcwd() + '/initial_data/'))
+    for crud_json in files:
+        if crud_json.rsplit('.', maxsplit=1)[1] == 'json':
 
-            kwargs = item['fields']
-            obj = model()
-            obj.set_fields(kwargs)
-            session.add(obj)
-            session.commit()
+            with open('./initial_data/'+ crud_json) as data_file:
+                print(crud_json)
+                data = json.load(data_file)
+
+                for item in data:
+                    model_relative_path = item['model']
+                    module_path, model_name = str(model_relative_path).rsplit('.', maxsplit=1)
+                    model = getattr(importlib.import_module(module_path), model_name)
+
+                    kwargs = item['fields']
+                    obj = model()
+                    obj.set_fields(kwargs)
+                    session.add(obj)
+                    session.commit()
 
 
 if __name__=='__main__':
