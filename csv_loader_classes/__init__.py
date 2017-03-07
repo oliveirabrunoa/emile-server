b923eec04b1ea138b943ece1adc6c089663a6da2
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-03-07 16:56:30 -0300

    Registro da subclasse loader do coursetype

--- csv_loader_classes/__init__.py
+++ csv_loader_classes/__init__.py
@@ -1,3 +1,4 @@
 CSV_LOADER_CLASSES = ['csv_loader_classes.program_loader.ProgramLoader',
+                      'csv_loader_classes.course_type_loader.CourseTypeLoader',
                       'csv_loader_classes.user_type_loader.UserTypeLoader',
                       'csv_loader_classes.users_loader.UsersLoader']
