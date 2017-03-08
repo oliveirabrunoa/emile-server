eceb3e1950fc4d437d9da2e66ddb926682b9955b
EliakinCosta <eliakim170@gmail.com>
2017-03-08 15:20:26 -0300

    Course Section csv loader criado

--- csv_loader_classes/__init__.py
+++ csv_loader_classes/__init__.py
@@ -3,4 +3,6 @@ CSV_LOADER_CLASSES = ['csv_loader_classes.program_loader.ProgramLoader',
                       'csv_loader_classes.course_section_students_status_loader.CourseSectionStudentsStatusLoader',
                       'csv_loader_classes.user_type_loader.UserTypeLoader',
                       'csv_loader_classes.users_loader.UsersLoader',
-                      'csv_loader_classes.courses_loader.CoursesLoader']
+                      'csv_loader_classes.courses_loader.CoursesLoader',
+                      'csv_loader_classes.course_sections_loader.CourseSectionsLoader',
+                      'csv_loader_classes.section_times_loader.SectionTimesLoader']
