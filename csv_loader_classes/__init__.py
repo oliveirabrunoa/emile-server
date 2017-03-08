7f5060b637f9e6507d64ece525c8ffea58108368
EliakinCosta <eliakim170@gmail.com>
2017-03-08 16:00:45 -0300

    Inclusao do csv loader de course_section_students e correcao de variavel incorreta em course_section_students_status_loader

--- csv_loader_classes/__init__.py
+++ csv_loader_classes/__init__.py
@@ -5,4 +5,5 @@ CSV_LOADER_CLASSES = ['csv_loader_classes.program_loader.ProgramLoader',
                       'csv_loader_classes.users_loader.UsersLoader',
                       'csv_loader_classes.courses_loader.CoursesLoader',
                       'csv_loader_classes.course_sections_loader.CourseSectionsLoader',
-                      'csv_loader_classes.section_times_loader.SectionTimesLoader']
+                      'csv_loader_classes.section_times_loader.SectionTimesLoader',
+                      'csv_loader_classes.course_section_students_loader.CourseSectionsStudentsLoader']
