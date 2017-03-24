1aa9b2dbac5ff29b9eec7f904474cc2e4ff7472f
EliakinCosta <eliakim170@gmail.com>
2017-03-24 16:14:41 -0300

    Manager para Program

--- cruds/crud_program/manager.py
+++ cruds/crud_program/manager.py
@@ -4,7 +4,7 @@ from cruds.crud_courses.models import Courses
 from cruds.crud_course_section_students.models import CourseSectionStudents
 from cruds.crud_course_sections.models import CourseSections
 from sqlalchemy import or_, func
-import importlib
+
 
 class Manager:
 
@@ -19,3 +19,23 @@ class Manager:
                                         filter(or_ (CourseSectionStudents.status == 2,
                                                     CourseSectionStudents.status == 3)).
                                         group_by(Courses.code,Courses.program_section).order_by(Courses.program_section).first())
+
+    def hours_and_credits_completed(self, student):
+        return (db.session.query(func.sum(Courses.hours), func.sum(Courses.credits)).
+                                        filter(CourseSectionStudents.course_section_id == CourseSections.id).
+                                        filter(CourseSections.course_id == Courses.id).
+                                        filter(Courses.program_id == models.Program.id).
+                                        filter(models.Program.id == student.program_id).
+                                        filter(CourseSectionStudents.user_id == student.id).
+                                        filter(CourseSectionStudents.status == 2).
+                                        group_by(models.Program.id).first())
+
+    def last_course_section_student(self, course, student):
+        return (db.session.query(CourseSectionStudents).
+                                        filter(CourseSectionStudents.course_section_id == CourseSections.id).
+                                        filter(CourseSections.course_id == Courses.id).
+                                        filter(Courses.program_id == models.Program.id).
+                                        filter(models.Program.id == student.program_id).
+                                        filter(CourseSectionStudents.user_id == student.id).
+                                        filter(Courses.id == course.id).
+                                        order_by(CourseSections.course_section_period.desc()).first())
