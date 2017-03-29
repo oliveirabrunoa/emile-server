1ea2955ebd3c0a6a9949c6567cbc464230849bff
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-03-29 14:05:45 -0300

    Alteração no retorno do response - serviço de login

--- cruds/crud_users/basicauthenticationbackend.py
+++ cruds/crud_users/basicauthenticationbackend.py
@@ -2,6 +2,7 @@ from . import models
 from flask import jsonify
 from cruds.crud_program.models import Program
 from cruds.crud_user_type.models import UserType
+from . import serializer
 
 
 class BasicAuthenticationBackend:
@@ -9,8 +10,7 @@ class BasicAuthenticationBackend:
     def authenticate(self, email, password):
         user = models.Users.query.filter_by(email=email, password=password).first()
         if user:
-            user_type = UserType.query.get(user.type)
-            program = Program.query.get(user.program_id)
-            return jsonify(user=dict(user.serialize(), type=user_type.serialize(),program_id=dict(id=program.id, abbreviation=program.abbreviation, name=program.name))), 200
+            user_serialized = serializer.UsersSerializer().serialize([user])
+            return jsonify(user=user_serialized), 200
         else:
             return jsonify(result='Invalid email or password'), 404
