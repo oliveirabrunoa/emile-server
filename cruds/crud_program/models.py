87e5371d9689382431d7e317aa68af09523f4318
oliveirabrunoa <oliveirabrunoa@gmail.com>
2017-04-06 14:53:59 -0300

    Definição de coordenador do curso

--- cruds/crud_program/models.py
+++ cruds/crud_program/models.py
@@ -12,7 +12,7 @@ class Program(db.Model):
     total_credits = db.Column(db.Integer)
     institution_id = db.Column(db.Integer, db.ForeignKey('institution.id'))
     courses = db.relationship('Courses')
-    #coordinator_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    coordinator_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     manager = manager.Manager()
 
 
@@ -22,3 +22,4 @@ class Program(db.Model):
         self.total_hours = fields['total_hours']
         self.total_credits = fields['total_credits']
         self.institution_id = fields['institution_id']
+        self.coordinator_id = fields['coordinator_id']
--- cruds/crud_program/serializer.py
+++ cruds/crud_program/serializer.py
@@ -2,6 +2,7 @@ from . import models
 from cruds.crud_institution.serializer import InstitutionSerializer
 from cruds.crud_institution.models import Institution
 from cruds.crud_courses.serializer import CoursesSerializer
+from cruds.crud_users.models import Users
 
 
 class ProgramSerializer:
@@ -11,6 +12,7 @@ class ProgramSerializer:
         for program in programs:
             institution = Institution.query.get(program.institution_id)
             courses =  program.courses
+            coordinator =  Users.query.get(program.coordinator_id)
 
             data.append({
                 'id': program.id,
@@ -19,7 +21,8 @@ class ProgramSerializer:
                 'total_hours': program.total_hours,
                 'total_credits':  program.total_credits,
                 'courses': CoursesSerializer().serialize(courses),
-                'institution': InstitutionSerializer().serialize([institution])
+                'institution': InstitutionSerializer().serialize([institution]),
+                'coordinator_id': dict(id=coordinator.id, email=coordinator.email, name=coordinator.name)
             })
 
         return data
